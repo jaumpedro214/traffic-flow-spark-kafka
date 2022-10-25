29aa7a2b58e0a8a302eec9bcbc815767ace71050
joaopedro214 <joaopedrodasilvalima@gmail.com>
2022-10-25 10:09:59 -0300

    FIX: Json to parquet job

--- src/transform_json_to_parquet.py
+++ src/transform_json_to_parquet.py
@@ -1,6 +1,7 @@
 
 from pyspark.sql import SparkSession
-from itertools import chain
+from pyspark.sql.types import StructType, StructField, StringType, LongType, TimestampType
+
 import os
 
 spark = SparkSession.builder\
@@ -11,7 +12,23 @@ spark = SparkSession.builder\
 # /data/AGOSTO_2022/20220801/20220801_00.json
 BASE_PATH = '/data/AGOSTO_2022/'
 
-WRITE_PATH = '/opt/bitnami/spark/data/AGOSTO_2022_PARQUET/'
+WRITE_PATH = '/data/AGOSTO_2022_PARQUET/'
+
+SCHEMA = StructType([
+    StructField("ID EQP", LongType()),
+    StructField("DATA HORA", TimestampType()),
+    StructField("MILESEGUNDO", LongType()),
+    StructField("FAIXA", LongType()),
+    StructField("ID DE ENDEREÇO", LongType()),
+    StructField("VELOCIDADE DA VIA", StringType()),
+    StructField("VELOCIDADE AFERIDA", StringType()),
+    StructField("TAMANHO", StringType()),
+    StructField("NUMERO DE SÉRIE", LongType()),
+    StructField("LATITUDE", StringType()),
+    StructField("LONGITUDE", StringType()),
+    StructField("ENDEREÇO", StringType()),
+    StructField("SENTIDO", StringType())
+])
 
 # Get all the folders from the base path
 folders = [os.path.join(BASE_PATH, f) for f in os.listdir(BASE_PATH)]
@@ -39,14 +56,18 @@ for folder, files in json_files.items():
         """
     )
     
-    spark.read.json(files)\
-    .repartition(1)\
-    .write.parquet(
-        os.path.join(WRITE_PATH, folder.split('/')[-1])
-    )
+    spark\
+    .read\
+    .option("multiline", "true")\
+    .json(files, schema=SCHEMA)\
+    .write\
+    .mode('overwrite')\
+    .parquet(os.path.join(WRITE_PATH, folder.split('/')[-1]))
 
 spark.stop()
 
 # Command to run the script
 # spark-submit --deploy-mode client --master spark://spark:7077 --driver-memory 2G --executor-memory 2G transform_json_to_parquet.py
+
+# Unprotect the folder
 # sudo chmod -R 777 data/ 
