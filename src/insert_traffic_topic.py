d9afa6c77ae18ec35e73e53bb4100d8e13005781
joaopedro214 <joaopedrodasilvalima@gmail.com>
2022-10-26 21:44:48 -0300

    Join parquet files, insert in kafka topic

---
+++ src/insert_traffic_topic.py
@@ -0,0 +1,47 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, LongType
+
+KAFKA_BOOTSTRAP_SERVERS = "kafka:9092"
+KAFKA_TOPIC = "traffic_sensor"
+FILE_PATH = "/data/AGOSTO_2022_PARQUET_FINAL/"
+
+# "ID EQP" -> INT 64
+
+SCHEMA = StructType([
+    StructField("ID EQP", LongType()),
+    StructField("DATA HORA", TimestampType()),
+    StructField("MILESEGUNDO", LongType()),
+    StructField("FAIXA", LongType()),
+    StructField("ID DE ENDEREÇO", LongType()),
+    StructField("VELOCIDADE DA VIA", StringType()),
+    StructField("VELOCIDADE AFERIDA", StringType()),
+    StructField("TAMANHO", StringType()),
+    StructField("NUMERO DE SÉRIE", LongType()),
+    StructField("LATITUDE", StringType()),
+    StructField("LONGITUDE", StringType()),
+    StructField("ENDEREÇO", StringType()),
+    StructField("SENTIDO", StringType())
+])
+
+spark = SparkSession.builder.appName("write_traffic_sensor_topic").getOrCreate()
+
+
+# Read the parquet file write it to the topic
+# We need to specify the schema in the stream
+# and also convert the entries to the format key, value
+df_traffic_stream = spark.readStream.format("parquet")\
+    .schema(SCHEMA)\
+    .load(FILE_PATH)\
+    .selectExpr("'mykey' AS key", "to_json(struct(*)) AS value")\
+    .limit(1000)\
+
+# Write the stream to the topic
+df_traffic_stream\
+    .writeStream\
+    .format("kafka")\
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)\
+    .option("topic", KAFKA_TOPIC)\
+    .option("checkpointLocation", "/tmp/checkpoint")\
+    .start()\
+    .awaitTermination()
--- src/join_parquet_files.py
+++ src/join_parquet_files.py
@@ -5,16 +5,18 @@ spark = SparkSession.builder\
 .appName('joinParquet')\
 .getOrCreate()
 
-BASE_PATH = '/opt/bitnami/spark/data/AGOSTO_2022_PARQUET/'
+BASE_PATH = '/data/AGOSTO_2022_PARQUET/'
 
 # Get all the files from the base path
 files = [os.path.join(BASE_PATH, f) for f in os.listdir(BASE_PATH)]
 
 spark.read.parquet(*files)\
-.repartition(1)\
+.repartition(20)\
 .write\
+.option('maxRecordsPerFile', 100000)\
+.mode('overwrite')\
 .parquet(
-    '/opt/bitnami/spark/data/AGOSTO_2022_PARQUET_FINAL/'
+    '/data/AGOSTO_2022_PARQUET_FINAL/'
 )
 
 spark.stop()
